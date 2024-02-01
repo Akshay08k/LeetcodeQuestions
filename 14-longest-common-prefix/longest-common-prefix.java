2e29ce27728a76e1913acb4c090d089a822ee3e7
Akshay Komale <96125015+Akshay08k@users.noreply.github.com>
2024-02-01 19:37:34 +0530

    Time: 1 ms (80.89%) | Memory: 41.6 MB (38.24%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -4,22 +4,18 @@ public class Solution {
             return "";
         }
 
-        // Take the first string as the reference
         String reference = strs[0];
 
         for (int i = 0; i < reference.length(); i++) {
             char currentChar = reference.charAt(i);
 
-            // Check if the character is common in all strings
             for (int j = 1; j < strs.length; j++) {
                 if (i >= strs[j].length() || strs[j].charAt(i) != currentChar) {
-                    // If not, return the prefix found so far
                     return reference.substring(0, i);
                 }
             }
         }
 
-        // If the loop completes, the entire reference string is the common prefix
         return reference;
     }
 }
