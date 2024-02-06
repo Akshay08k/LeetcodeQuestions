ee08edabbfc89215f522e5180071a33de4fc1d1c
Akshay Komale <96125015+Akshay08k@users.noreply.github.com>
2024-02-06 18:24:08 +0530

    Time: 4 ms (86.63%) | Memory: 68.7 MB (29.00%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true; // An empty list or a single-node list is a palindrome
+        }
+
+        // Find the middle of the linked list
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse the second half of the linked list
+        ListNode secondHalf = reverseList(slow);
+
+        // Compare the first half with the reversed second half
+        while (secondHalf != null) {
+            if (head.val != secondHalf.val) {
+                return false; // Not a palindrome
+            }
+            head = head.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true; // It is a palindrome
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev; // 'prev' is the new head of the reversed list
+    }
+}
